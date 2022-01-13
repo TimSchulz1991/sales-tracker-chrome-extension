5a1efe75bb464663520a5783acbad7656ca413f0
Tim Schulz <tim@getbej.com>
2022-01-13 21:09:34 +0000

    Grab HTML elements and add event listener

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", () => {
+    myLeads.push(inputEl.value);
+})
+
+for (let lead of myLeads) {
+    ulEl.innerHTML += `<li>${lead}</li>`;
+}
