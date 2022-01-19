fc2d1aeb1a1a02e257d2338e6551cafe2e8b8833
Tim Schulz <tim@getbej.com>
2022-01-19 15:43:20 +0000

    Add icon and json for deployment

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,19 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
 })
 
-for (let lead of myLeads) {
-    ulEl.innerHTML += `<li>${lead}</li>`;
+const renderLeads = () => {
+    let listItems = "";
+    for (let lead of myLeads) {
+        listItems += `
+            <a href="${lead}" target="_blank">
+                <li>
+                    ${lead}
+                </li>
+            </a>`;
+    }
+    ulEl.innerHTML = listItems;
 }
